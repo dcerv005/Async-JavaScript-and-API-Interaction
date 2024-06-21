a85a035f39752aea20c94fd5336eaf91a7b82742
dcerv005 <163106687+dcerv005@users.noreply.github.com>
2024-06-21 12:56:32 -0500

    Update question1.js
    second submit

--- question1.js
+++ question1.js
@@ -1,10 +1,14 @@
 async function fetchCharacters(character) {
-    const url="https://gateway.marvel.com:443/v1/public/characters?name="+character+"&apikey=1db16fa474c3fdf9e74d956d788ea55f"
+    const publicKey="1db16fa474c3fdf9e74d956d788ea55f"
+    const hash="e1c499a5ceb1d8ab8e4afbeb45348103";
+    const url="http://gateway.marvel.com/v1/public/characters?name="+character+"&ts=1&apikey="+publicKey+"&hash="+hash;
+    
     try{
         
         const response= await fetch(url);
             
         const data = await response.json();
+        console.log(data);
         return data;
     }catch(error){
         console.error(error);
@@ -12,8 +16,10 @@ async function fetchCharacters(character) {
 }
 
 async function addToDocument(data) {
+    const heroData= await data;
+    console.log(heroData.data.results[0].name)
     const userInfoElement=document.getElementById("characterInfo");
-    userInfoElement.innerHTML= '<h2>'+data+'</h2>';
+    userInfoElement.innerHTML= '<h2>'+heroData.data.results[0].name+'</h2> <p><strong>Description</strong>: '+heroData.data.results[0].description;
 }
 
 addToDocument(fetchCharacters('hulk'));
